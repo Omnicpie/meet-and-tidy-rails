ca155ffc1dad0b4cf78fb7afca5df4933a7d26b8
Ian Fleeton <ianfleeton@gmail.com>
2021-02-20 15:55:58 +0000

    Add event query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,6 +7,15 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
+    field :event, EventType, null: true do
+      description "Find an event by ID"
+      argument :id, ID, required: true
+    end
+
+    def event(id:)
+      Event.find(id)
+    end
+
     field :events, [EventType], null: true do
       description "All events"
     end
