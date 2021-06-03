e38b0ea358ba537f36378129b9b9d33b65401e8d
Beth Hubbard <beth.hubbard@commit.digital>
2021-06-03 11:12:14 +0100

    Add images to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,29 @@ Facility.destroy_all
 Mess.destroy_all
 User.destroy_all
 
+puts "\n== Creating admins =="
+
 Administrator.create!(
   email: "tidy@example.com",
   name: "Tidy",
   password: "top-secret"
 )
+
+puts "\n== Creating users =="
+
+User.create!(
+  email: "morty@example.com",
+  name: "Morty",
+  password: "beeblebrox"
+)
+
+User.create!(
+  email: "pam@example.com",
+  name: "Pam",
+  password: "beeblebrox"
+)
+
+puts "\n== Creating event types =="
 
 EventType.create!([
   {name: "Canal"},
@@ -39,6 +57,8 @@ event_type_c = EventType.create!(
   name: "Beach"
 )
 
+puts "\n== Creating facilities =="
+
 Facility.create!([
   {name: "Toilets"},
   {name: "Parking"},
@@ -54,47 +74,80 @@ facility_b = Facility.create!(
   name: "Child friendly"
 )
 
-Event.create!(
+puts "\n== Creating events =="
+
+event_1 = Event.create!(
   title: "Urban Park Clean up!",
   event_types: [event_type_a, event_type_b],
   description: "A chance to clean up our local streets and give back to the community.",
   location: "Leeds",
   date: 11-06-2021,
-  url: "https://www.google.com"
+  url: "https://www.google.com",
 )
 
-Event.create!(
+event_2 = Event.create!(
   title: "Redcar Beach Clean up!",
   event_types: [event_type_c],
   description: "Time to do your part and clean up the beach!",
   location: "Redcar",
   date: 15-12-2021,
-  url: "https://www.google.com"
+  url: "https://www.google.com",
 )
 
-Event.create!(
+event_3 = Event.create!(
   title: "West Park Clean up!",
   event_types: [event_type_a],
   description: "A chance to clean up our local streets and give back to the community.",
   location: "Goole",
   date: 23-9-2021,
-  url: "https://www.google.com"
+  url: "https://www.google.com",
 )
 
-User.create!(
-  email: "morty@example.com",
-  name: "Morty",
-  password: "beeblebrox"
-)
+# puts "\n== Creating mess types =="
 
-User.create!(
-  email: "pam@example.com",
-  name: "Pam",
-  password: "beeblebrox"
-)
+# mess_type_a = MessType.create!(
+#   name: "Fly-tip"
+# )
 
-Mess.create!(
+# mess_type_b = MessType.create!(
+#   name: "Rubbish"
+# )
+
+# mess_type_c = MessType.create!(
+#   name: "Other"
+# )
+
+puts "\n== Creating messes =="
+
+mess_1 = Mess.create!(
   title: "Bin bags on George St",
+  # mess_types: [mess_type_b],
   description: "Someone has left all their rubbish on George Street and it needs cleaning up.",
   location: "George Street"
 )
+
+mess_2 = Mess.create!(
+  title: "Fly-tip in alley",
+  # mess_types: [mess_type_a],
+  description: "Massive fly-tip on the alley between Carilse and Smitth street.",
+  location: "Doncaster"
+)
+
+puts "\n== handinling active images =="
+
+def path_for_image(filename)
+  "app/assets/images/#{filename}"
+end
+
+# ActiveStorage attachments
+event_1.images.attach(io: File.open(path_for_image("urban.jpg")), filename: "urban.jpg")
+event_1.save!
+event_2.images.attach(io: File.open(path_for_image("west_park.jpg")), filename: "west_park.jpg")
+event_2.save!
+event_3.images.attach(io: File.open(path_for_image("redcar.jpg")), filename: "redcar.jpg")
+event_3.save!
+mess_1.images.attach(io: File.open(path_for_image("rubbish.jpg")), filename: "rubbish.jpg")
+mess_1.save!
+mess_2.images.attach(io: File.open(path_for_image("fly_tip.jpg")), filename: "fly_tip.jpg")
+mess_2.save!
+
