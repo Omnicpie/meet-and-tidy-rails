ecf0a3d7142088aefb8b9f036df2fbe3899acf12
Beth Hubbard <beth.hubbard@commit.digital>
2021-06-03 12:00:02 +0100

    Update production enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "meet_and_tidy_rails_production"
 
+  config.action_mailer.default_url_options = {host: "meet-and-tidy.commit.digital", protocol: "https"}
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
