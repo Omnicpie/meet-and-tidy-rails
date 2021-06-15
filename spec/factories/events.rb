3458cbfb6fa4839f7d190d7a02f7c7c399907731
Beth Hubbard <bah1996@hotmail.co.uk>
2021-06-15 15:41:43 +0100

    Update event factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :event do
     sequence(:title) { |n| "Event #{n}" }
+    url { "www.google.com" }
+    location { "Leeds" }
+    description { "Some description" }
   end
 end
