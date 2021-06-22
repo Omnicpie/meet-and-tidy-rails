73b7550d441df11a7efc23405e5ccbe9f677ac3c
Beth Hubbard <bah1996@hotmail.co.uk>
2021-06-22 15:40:01 +0100

    Event creation mutation/spec wip

--- app/graphql/mutations/create_event.rb
+++ app/graphql/mutations/create_event.rb
@@ -4,18 +4,19 @@ class Mutations::CreateEvent < Mutations::BaseMutation
   argument :date, String, required: false
   argument :description, String, required: false
   argument :event_type_id, ID, required: true
+  argument :facility_id, ID, required: true
   argument :location, String, required: false
   argument :title, String, required: true
   argument :url, String, required: false
 
   field :errors, [String], null: false
 
-  def resolve(date:, description:, event_type_id:, location:, title:, url:)
-    Rails.logger.debug('xxxxxxxxxx')
+  def resolve(date:, description:, event_type_id:, facility_id:, location:, title:, url:)
     event = Event.new(
       date: date,
       description: description,
       event_type_id: event_type_id,
+      facility_id: facility_id,
       location: location,
       title: title,
       url: url,
--- spec/graphql/mutations/create_event_spec.rb
+++ spec/graphql/mutations/create_event_spec.rb
@@ -2,13 +2,14 @@ require "rails_helper"
 
 module Mutations
   RSpec.describe CreateEvent, type: :request do
-    def query(date:, description:, eventTypeId:, location:, title:, url:)
+    def query(date:, description:, eventTypeId:, facilityId:, location:, title:, url:)
       <<~GQL
         mutation {
           createEvent (
             date: "#{date}"
             description: "#{description}"
             eventTypeId: "#{eventTypeId}"
+            facilityId: "#{facilityId}"
             location: "#{location}"
             title: "#{title}"
             url: "#{url}"
@@ -21,11 +22,13 @@ module Mutations
 
     it "creates a new event" do
       event_type = FactoryBot.create(:event_type)
+      facility = FactoryBot.create(:facility)
       post "/graphql", params: {
         query: query(
           date: "2021-07-23",
           description: "An exciting chance to help out our communuty!",
           eventTypeId: event_type.id,
+          facilityId: facility.id,
           location: "East park, Hull",
           title: "East Park clean up!",
           url: "www.google.com",
@@ -37,6 +40,7 @@ module Mutations
       expect(event.date).to eq "2021-07-23"
       expect(event.description).to eq "An exciting chance to help out our communuty!"
       expect(event.event_type_id).to eq event_type.id
+      expect(event.facility_id).to eq facility.id
       expect(event.location).to eq "East park, Hull"
       expect(event.title).to eq "East Park clean up!"
       expect(event.url).to eq "www.google.com"
