30195bfdf21af9b9e467b600343878b5cf5aeead
Beth Hubbard <bah1996@hotmail.co.uk>
2021-06-22 12:22:37 +0100

    Fix create event mutation spec

--- spec/graphql/mutations/create_event_spec.rb
+++ spec/graphql/mutations/create_event_spec.rb
@@ -2,13 +2,13 @@ require "rails_helper"
 
 module Mutations
   RSpec.describe CreateEvent, type: :request do
-    def query(date:, description:, event_type_id:, location:, title:, url:)
+    def query(date:, description:, eventTypeId:, location:, title:, url:)
       <<~GQL
         mutation {
           createEvent (
             date: "#{date}"
             description: "#{description}"
-            event_type_id: "#{event_type_id}"
+            eventTypeId: "#{eventTypeId}"
             location: "#{location}"
             title: "#{title}"
             url: "#{url}"
@@ -19,25 +19,24 @@ module Mutations
       GQL
     end
 
-    it "creates a new event account" do
+    it "creates a new event" do
+      event_type = FactoryBot.create(:event_type)
       post "/graphql", params: {
         query: query(
           date: "2021-07-23",
           description: "An exciting chance to help out our communuty!",
-          event_type_id: "Park",
+          eventTypeId: event_type.id,
           location: "East park, Hull",
           title: "East Park clean up!",
           url: "www.google.com",
         )
       }
 
-      puts response.status
-      puts response.body
       expect(Event.count).to eq 1
       event = Event.last
       expect(event.date).to eq "2021-07-23"
       expect(event.description).to eq "An exciting chance to help out our communuty!"
-      expect(event.event_type_id).to eq "Park"
+      expect(event.event_type_id).to eq event_type.id
       expect(event.location).to eq "East park, Hull"
       expect(event.title).to eq "East Park clean up!"
       expect(event.url).to eq "www.google.com"
