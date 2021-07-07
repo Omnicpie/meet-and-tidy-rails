bd48e9d37a9094f57d56198445e9b02c95b9780d
Beth Hubbard <beth.hubbard@commit.digital>
2021-07-07 09:05:57 +0100

    Clean up event type query

--- app/graphql/types/event_type.rb
+++ app/graphql/types/event_type.rb
@@ -1,5 +1,6 @@
 module Types
   class EventType < Types::BaseObject
+    field :description, String, null: true
     field :id, ID, null: false
     field :title, String, null: false
     field :url, String, null: true
@@ -7,12 +8,10 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
-    field :description, String, null: true
-
-    field :event_type, [EventTypeType], null: true
-
-    field :image_urls, [String], null: true
     field :attendances, [AttendanceType], null: false
+    field :event_type, [EventTypeType], null: true
+    field :facilities, [FacilityType], null: true
+    field :image_urls, [String], null: true
   end
 end
 
