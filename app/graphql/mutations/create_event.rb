ad01083defbd2c0eb05f54ce422d006a5d470743
Beth Hubbard <beth.hubbard@commit.digital>
2021-07-07 14:42:43 +0100

    Add facility ids to create event mutation wip
    Co-authored-by: ianfleeton@gmail.com <ianfleeton@gmail.com>
    Co-authored-by: georginavweston <georginavweston@users.noreply.github.com>

--- app/graphql/mutations/create_event.rb
+++ app/graphql/mutations/create_event.rb
@@ -4,23 +4,28 @@ class Mutations::CreateEvent < Mutations::BaseMutation
   argument :date, String, required: false
   argument :description, String, required: false
   argument :event_type_id, ID, required: true
-  argument :facility_id, ID, required: true
+  argument :facility_ids, [ID], required: true
   argument :location, String, required: false
   argument :title, String, required: true
   argument :url, String, required: false
 
   field :errors, [String], null: false
 
-  def resolve(date:, description:, event_type_id:, facility_id:, location:, title:, url:)
+  def resolve(date:, description:, event_type_id:, facility_ids:, location:, title:, url:)
     event = Event.new(
       date: date,
       description: description,
       event_type_id: event_type_id,
-      facility_id: facility_id,
+#      facility_id: facility_id,
       location: location,
       title: title,
       url: url,
     )
+    facility_ids.each do |facility_id|
+      if (facility = Facility.find_by(id: facility_id))
+        event.facilities << facility
+      end
+    end
     if event.save
       {
         errors: []
